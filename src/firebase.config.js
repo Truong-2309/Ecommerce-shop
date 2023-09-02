fed0f2e67e689709da02415b5416a16349485a1d
Truong-2309 <93892994+Truong-2309@users.noreply.github.com>
2023-09-02 16:50:46 +0700

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKb5ZZy1SxOh_M2cD8DNcO-nRfSm3Q0MM",
+  authDomain: "bazarapp-a4849.firebaseapp.com",
+  projectId: "bazarapp-a4849",
+  storageBucket: "bazarapp-a4849.appspot.com",
+  messagingSenderId: "815416893601",
+  appId: "1:815416893601:web:ae589c420998a1e42890e0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/pages/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Shop = ({shop}) => {
+  return (
+    <div>
+        Shop
+    </div>
+  )
+}
+
+export default Shop
